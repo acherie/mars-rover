6ef5b13c9ac370aa46ddfe8866bb695656ac4a2b
wangjie <wangjie@d-bigdata.com>
2018-05-12 12:57:33 +0800

    Position should properly init

---
+++ src/main/java/acherie/marsrover/Position.java
@@ -0,0 +1,31 @@
+package acherie.marsrover;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.ToString;
+
+/**
+ * @author wangjie
+ * @date 2018/5/12
+ */
+@ToString
+@EqualsAndHashCode
+public class Position {
+
+    @Getter
+    private int x;
+    @Getter
+    private int y;
+    @Getter
+    private String p;
+
+    public Position(int x, int y, String p) {
+        this.x = x;
+        this.y = y;
+        this.p = p;
+    }
+
+    public String position() {
+        return String.format("%d %d %s", this.x, this.y, this.p);
+    }
+}
---
+++ src/test/java/acherie/marsrover/PositionTest.java
@@ -0,0 +1,21 @@
+package acherie.marsrover;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * @author wangjie
+ * @date 2018/5/12
+ */
+public class PositionTest {
+
+    @Test
+    public void Position_ShouldConcat_WhenInit() {
+        // given
+        Position position = new Position(0, 0, "N");
+        // when
+        String positionStr = position.position();
+        // then
+        Assert.assertEquals(positionStr, "0 0 N");
+    }
+}
